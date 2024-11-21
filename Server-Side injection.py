2768ce77d2fc95981acf1f3ccefa88cd65b26a31
wifi96h <dillon.wifi96@gmail.com>
2024-11-21 09:35:07 -0500

    Create Server-Side injection.py

---
+++ Server-Side injection.py
@@ -0,0 +1,11 @@
+### Directory Traversal/Path Traversal
+### Ability to read/execute outside web server’s directory
+### Uses ../../ (relative paths) in manipulating a server-side file path
+### view_image.php?file=../../etc/passwd
+ 
+../../../../../../../../../../../../../../etc/passwd ### you just put a bunch so you get back to the top 
+### you can do this with any file ie /etc/host 
+###
+### ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ post method 
+### change the url is considered the get method 
+### try all of the input feilds as only som may be considered misconfigured 
